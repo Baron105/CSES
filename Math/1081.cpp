25d3d6fdfeaf1a931887f4bc16e892ff493a3145
Baron105 <barunparua@gmail.com>
2023-04-17 14:39:55 +0530

    finally got it to work, but it's not the most efficient solution

--- Math/1081.cpp
+++ Math/1081.cpp
@@ -1,30 +1,38 @@
 #include <iostream>
-#include <algorithm>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
 
-int main() {
-    int n;
-    cin >> n;
-    vector<int> a(n);
-    for (int i = 0; i < n; i++) {
-        cin >> a[i];
+int main()
+{
+    const int x = 1e6;
+    int N, a;
+    vector<int> d(x + 1, 0);
+
+    cin >> N;
+
+    for (int i = 0; i < N; i++)
+    {
+        cin >> a;
+        d[a]++;
     }
-    sort(a.begin(), a.end());
+
     int ans = 0;
-    for (int g = a[n-1]; g >= 1; g--) {
+
+    for (int i = x; i >= 1; i--)
+    {
         int cnt = 0;
-        for (int i = 0; i < n; i++) {
-            if (a[i] % g == 0) {
-                cnt++;
-            }
-        }
-        if (cnt >= 2) {
-            ans = g;
+        for (int j = i; j <= x; j += i)
+            cnt += d[j];
+
+        if (cnt >= 2)
+        {
+            ans = i;
             break;
         }
     }
+
     cout << ans << endl;
     return 0;
 }
